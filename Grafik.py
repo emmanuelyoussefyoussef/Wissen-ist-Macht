69ccffaa0ee2ff62462f55e0d7d14dc15de36757
youssef youssef <youssef.youssef@kardex.com>
2024-01-24 14:11:41 +0100

    placeholder for questions

--- Grafik.py
+++ Grafik.py
@@ -13,6 +13,8 @@ player_name.place(anchor="nw")
 score = tk.Label(window,text='Score:'+str(Score),font=('Arial',15))
 score.pack(anchor="ne")
 
+question = tk.Label(window,text='placeholder for the question',font=('Arial',30))
+question.pack(anchor='center',pady=50)
 
 
 
