c571be26fa196d7e9b3e0759ab931356c1ee7cfd
youssef youssef <youssef.youssef@kardex.com>
2024-01-24 20:53:45 +0100

    answers are now reactive

--- Grafik.py
+++ Grafik.py
@@ -3,7 +3,7 @@ import random
 import time
 
 Score = 0 
-antworten=['a','b','c','d']
+frage="placeholder for the question"
 #Score update funktion
 def update_score(key):
     global Score
@@ -11,7 +11,21 @@ def update_score(key):
         Score = int(random.random()*100)
         score.config(text='Score: ' + str(Score))
     #window.after(1000,update_score)
+def antwort():
+    global frage
+    frage = "Richtig"
+    question.config(text=frage)
 
+def falsche_antwort():
+    global frage
+    frage="Falsch HAHAHAHAHA"
+    question.config(text=frage)
+
+def reset_frage():
+    global frage
+    frage="placeholder for the question"
+    question.config(text=frage)
+    window.after(1500,reset_frage)
 
 
 current_player=input('Player name:')
@@ -30,28 +44,28 @@ score = tk.Label(window,text='Score:'+str(Score),font=('Arial',15))
 score.grid(row=0,column=3)
 
 #Fragen_funktion
-question = tk.Label(window,text='placeholder for the question',font=('Arial',20),bg="beige")
+question = tk.Label(window,text=frage,font=('Arial',20),bg="beige")
 question.grid(row=1,column=0,columnspan=4)
 
 #---------------------------------------------------------------------------------------------
 
 #Tasten
-Buttona= tk.Button(window,text='Antowrt A',font=('Arial',14),width=25,height=1,command=lambda:update_score('A'))
+Buttona= tk.Button(window,text='A: ich bin die richtige Antwort vertraue mir',font=('Arial',14),bg='#ff6666',width=40,height=1,command=lambda:(update_score('A'),antwort()))
 Buttona.grid(row=2,column=0)
 
-Buttonb= tk.Button(window,text='Antowrt B',font=('Arial',14),width=25,height=1,command=lambda:update_score('B'))
+Buttonb= tk.Button(window,text='B: ich bin leider falsch',font=('Arial',14),bg='#458B74',width=40,height=1,command=lambda:(update_score('B'),falsche_antwort()))
 Buttonb.grid(row=2,column=3)
 
-Buttonc= tk.Button(window,text='Antowrt C',font=('Arial',14),width=25,height=1,command=lambda:update_score('C'))
+Buttonc= tk.Button(window,text='C: ich bin auch leider falsch',font=('Arial',14),bg='skyblue',width=40,height=1,command=lambda:(update_score('C'),falsche_antwort()))
 Buttonc.grid(row=3,column=0)
 
-Buttond= tk.Button(window,text='Antowrt D',font=('Arial',14),width=25,height=1,command=lambda:update_score('D'))
+Buttond= tk.Button(window,text='D: versuch mich nicht',font=('Arial',14),bg='#a37d00',width=40,height=1,command=lambda:(update_score('D'),falsche_antwort()))
 Buttond.grid(row=3,column=3)
 
 
 
 #diese funktion ruft jede sekunde die update_score funktion auf
-#window.after(1000,update_score)
+window.after(1500,reset_frage)
 
 window.mainloop()
 
