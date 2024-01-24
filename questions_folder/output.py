9858714aeaf30c9c738980469d7a395ca83ff3a1
larsrmk <152874086+larsrmk@users.noreply.github.com>
2024-01-24 22:48:21 +0100

    Create output.py

---
+++ questions_folder/output.py
@@ -0,0 +1,18 @@
+def start_quiz():
+    questions_dict = questions.get_random_questions()
+
+    for question_number, question_info in questions_dict.items():
+        question = question_info['question']
+        answers = question_info['possible_answers']
+        correct_answer = question_info['right_answer']
+        image = question_info['picture']
+        Category = question_info['category']
+
+        print(f'\nFrage {question_number} ({Category}): {question}')
+        for i, answer in enumerate(answers, start=1):
+            print(f'{i}. {answer}')
+
+        user_answer = input('Antwort eingeben (1-4): ')
+
+if __name__ == '__main__':
+    start_quiz()
