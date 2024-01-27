c04d8540d8e4b8357f35431021bfac5da8ba960e
youssef youssef <youssef.youssef@kardex.com>
2024-01-28 00:07:31 +0100

    Das spiel ist jetzt spielbereit

--- Grafik.py
+++ Grafik.py
@@ -26,26 +26,40 @@ key,imported_questions=get_random_questions()
 
 #dies ist die Liste mit den 10 fragen die angezeigt werden sollen
 shown_questions=''
+correct_questions=''
+answer=''
+possible_answer=''
 
-
+def auswahl():
+    global possible_answer
+    possible_answer=imported_questions[key[index]]['possible_answers']
+    Buttona.config(text=possible_answer[0])
+    Buttonb.config(text=possible_answer[1])
+    Buttonc.config(text=possible_answer[2])
+    Buttond.config(text=possible_answer[3])
 
 
 
 def neue_frage():
     global imported_questions
-    global index
+    global correct_questions
+    global answer
     shown_questions=imported_questions[key[index]]['question']
     print(index,key[index],shown_questions)
-    index = index +1
     question.config(text=shown_questions)
+    auswahl()
+    
 
 
 
 def erste_frage():
     global boole
+    global possible_answer
     if boole ==True:
         neue_frage()
         boole=False
+        possible_answer=imported_questions[key[index]]['possible_answers']
+
 
 #Score update funktion
 def update_score(key):
@@ -57,33 +71,40 @@ def update_score(key):
 
 
 #Diese Funktion zeigt das Ergebnis Richtig an
-def antwort():
+def richtige_antwort():
     window.bell()
     update_score('A')
     question.config(text="Richtig")
     window.after(1000,neue_frage)
+    if index ==9:
+        window.after(2000, ende)#Am ende wird Richtig oder falsch angezeigt und erst nach 2 sekunden wird das Ergebnis angezeigt
 
 
 
 
 #diese funktion prüft ab ob die eingabe des spielers richtig ist    
-def scanner(key):
-    if index<10:
-        if key=='A':
-            antwort()
+def scanner(var):
+    global index
+    if index<=9:
+        if var==imported_questions[key[index]]['question_answer']:
+            richtige_antwort()
         else:falsche_antwort()
     else:
         ende()
+    index = index +1
 
 
 
 
 #Diese Funktion zeigt das Ergebnis Falsch an
 def falsche_antwort():
+    global index
+    window.bell()
     question.config(text="Falsch HAHAHAHAHA")
-    Score = 0
-    score.config(text='Score: ' + str(Score))
+    #score.config(text='Score: ' + str(Score))
     window.after(1000,neue_frage)
+    if index ==9:
+        window.after(2000,ende)
 
 
 
@@ -96,7 +117,7 @@ def reset_frage():
 
 def ende():
     endergebnis=Score
-    question.config(text=endergebnis)
+    question.config(text='Dein Score beträgt:'+str(endergebnis))
     #window.after(5000,window.destroy)
     
 
@@ -134,17 +155,18 @@ abstandmitte= tk.Label(window,text='',width=40,height=5)
 abstandmitte.grid(row=4,column=2)
 
 #Antwort Blöcke
-Buttona= tk.Button(window,text='A: ich bin die richtige Antwort vertraue mir',font=('Arial',14),bg='#ff6666',width=40,height=5,command=lambda:(scanner('A')))
+Buttona= tk.Button(window,text='A: ich bin die richtige Antwort vertraue mir',font=('Arial',14),bg='#ff6666',width=40,height=5,command=lambda:(scanner(possible_answer[0])))
 Buttona.grid(row=4,column=0,padx=30)
 
-Buttonb= tk.Button(window,text='B: ich bin leider falsch',font=('Arial',14),bg='#458B74',width=40,height=5,command=lambda:(update_score('B'),scanner('B')))
+Buttonb= tk.Button(window,text='B: ich bin leider falsch',font=('Arial',14),bg='#458B74',width=40,height=5,command=lambda:scanner(possible_answer[1]))
 Buttonb.grid(row=4,column=4)
 
-Buttonc= tk.Button(window,text='C: ich bin auch leider falsch',font=('Arial',14),bg='skyblue',width=40,height=5,command=lambda:(update_score('C'),scanner('C')))
+Buttonc= tk.Button(window,text='C: ich bin auch leider falsch',font=('Arial',14),bg='skyblue',width=40,height=5,command=lambda:scanner(possible_answer[2]))
 Buttonc.grid(row=5,column=0)
 
-Buttond= tk.Button(window,text='D: versuch mich nicht',font=('Arial',14),bg='#a37d00',width=40,height=5,command=lambda:(update_score('D'),scanner('D')))
+Buttond= tk.Button(window,text='D: versuch mich nicht',font=('Arial',14),bg='#a37d00',width=40,height=5,command=lambda:scanner(possible_answer[3]))
 Buttond.grid(row=5,column=4)
+
 
 
 
